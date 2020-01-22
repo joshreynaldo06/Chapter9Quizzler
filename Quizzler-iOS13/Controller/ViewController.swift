7236a05c614a536cd7f94cfcc38c2b409c357d93
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-22 14:24:58 +0700

    second commit

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionTextLabel: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func trueButtonClicked(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func falseButtonClicked(_ sender: UIButton){
+        
+    }
 
 }
 
